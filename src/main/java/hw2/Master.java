903dba79c20f862d3114257864059dda78385d4c
Abriel <abrielz11@gmail.com>
2025-01-12 23:57:25 +0300

    2nd try

---
+++ src/main/java/hw2/Master.java
@@ -0,0 +1,8 @@
+package hw2;
+
+public class Master {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/hw2/model/Consumer.java
@@ -0,0 +1,54 @@
+package hw2.model;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Consumer implements Runnable {
+
+    private final static int PORT = 8080;
+
+    @Override
+    public void run() {
+
+        for (int i = 0; i < 10; i++) {
+            this.runner(PORT + i);
+        }
+    }
+
+
+    public void runner(int port) {
+
+        ServerSocket serverSocket;
+
+          try {
+                serverSocket = new ServerSocket(PORT);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+
+        try (Socket socket = serverSocket.accept();
+             InputStreamReader isr = new InputStreamReader(socket.getInputStream());
+             BufferedReader in = new BufferedReader(isr);
+             PrintWriter out = new PrintWriter(socket.getOutputStream(), true)
+        ) {
+            System.out.println("Connection accepted");
+
+            String line;
+            while ((line = in.readLine()) != null) {
+                System.out.println("Server received: " + line + ". Sending to client");
+                out.println(line);
+
+                if (line.equals("Bye")) {
+                    break;
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
